a7f5581bd10cbba63f1457a288404a8c61af8a3f
TreyPoynter <treypoynter@gmail.com>
2024-01-22 10:39:14 -0600

    Add project files.

---
+++ RetroRealm/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RetroRealm.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
