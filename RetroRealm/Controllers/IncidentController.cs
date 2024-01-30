e6b8a7dfa6134441b31f5c0019c07b23315db867
TreyPoynter <treypoynter@gmail.com>
2024-01-30 13:45:17 -0600

    Incident adding UI
    still need to add functionality

--- RetroRealm/Controllers/IncidentController.cs
+++ RetroRealm/Controllers/IncidentController.cs
@@ -1,13 +1,58 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RetroRealm.Migrations;
+using RetroRealm.Models;
 
 namespace RetroRealm.Controllers
 {
     public class IncidentController : Controller
     {
+        public GameModelContext Context { get; set; }
+        public IncidentController(GameModelContext ctx) => Context = ctx;
+
         [HttpGet]
         public IActionResult List()
         {
-            return View();
+            List <IncidentModel> incidents = Context.Incidents.Include(i => i.Customer).Include(i => i.Game).ToList();
+            return View(incidents);
+        }
+        [HttpGet]
+        public IActionResult Add()
+        {
+            ViewBag.Action = "Add";
+            ViewBag.Customers = Context.Customers.OrderBy(c => c.Firstname).ToList();
+            ViewBag.Games = Context.Games.OrderBy(g => g.Title).ToList();
+            ViewBag.Technicians = Context.Technicians.OrderBy(t => t.Name).ToList();
+            return View("Edit", new IncidentModel());
+        }
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            ViewBag.Action = "Edit";
+            ViewBag.Customers = Context.Customers.OrderBy(c => c.Firstname).ToList();
+            ViewBag.Games = Context.Games.OrderBy(g => g.Title).ToList();
+            ViewBag.Technicians = Context.Technicians.OrderBy(t => t.Name).ToList();
+            IncidentModel? incident = Context.Incidents.Find(id);
+            return View("Edit", incident);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(IncidentModel incident)
+        {
+            ViewBag.Customers = Context.Customers.OrderBy(c => c.Firstname).ToList();
+            ViewBag.Games = Context.Games.OrderBy(g => g.Title).ToList();
+            ViewBag.Technicians = Context.Technicians.OrderBy(t => t.Name).ToList();
+            if (ModelState.IsValid)
+            {
+                if (incident.IncidentModelId == 0)
+                    Context.Incidents.Add(incident);
+                else
+                    Context.Incidents.Update(incident);
+                Context.SaveChanges();
+                return RedirectToAction("List");
+            }
+            ViewBag.Action = (incident.IncidentModelId == 0) ? "Add" : "Edit";
+            return View(incident);
         }
     }
 }
--- RetroRealm/Models/IncidentModel.cs
+++ RetroRealm/Models/IncidentModel.cs
@@ -11,15 +11,16 @@ namespace RetroRealm.Models
         [Required(ErrorMessage = "A description is required!")]
         public string? Description { get; set; }
         [Required(ErrorMessage = "Open date is required!")]
+        [Range(typeof(DateTime), "1900-01-01", "{0:yyyy-MM-dd}", ErrorMessage = "Date must be on or before the current date.")]
         public DateTime? DateOpened { get; set; }
         public DateTime? DateClosed { get; set; }
 
         [Required (ErrorMessage = "A customer is required!")]
-        public int CustomerModelId { get; set; }
+        public int? CustomerModelId { get; set; }
         [Required(ErrorMessage = "A game is required!")]
-        public int GameModelId { get; set; }
+        public int? GameModelId { get; set; }
         [Required(ErrorMessage = "A technician is required!")]
-        public int TechnicianModelId { get; set; }
+        public int? TechnicianModelId { get; set; }
 
         [ValidateNever]
         public CustomerModel Customer { get; set; }
