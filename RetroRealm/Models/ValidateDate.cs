132084c6f30d58d38bdac3ddd630ffc7dfc22c59
TreyPoynter <treypoynter@gmail.com>
2024-01-31 09:41:11 -0600

    Create ValidateDate.cs

---
+++ RetroRealm/Models/ValidateDate.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RetroRealm.Models
+{
+    public class ValidateDate : ValidationAttribute
+    {
+        protected override ValidationResult IsValid
+                         (object obj, ValidationContext validationContext)
+        {
+            DateTime date = Convert.ToDateTime(obj);
+            return (date <= DateTime.UtcNow && date >= DateTime.UtcNow.AddYears(-1))
+                ? ValidationResult.Success
+                : new ValidationResult("Invalid date range");
+        }
+    }
+}
