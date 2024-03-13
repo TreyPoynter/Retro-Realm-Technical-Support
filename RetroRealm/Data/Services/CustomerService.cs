ffa54c5084bc7f2b1f7e02f767a548bdb46b909a
TreyPoynter <treypoynter@gmail.com>
2024-03-13 11:58:20 -0500

    created customer service

---
+++ RetroRealm/Data/Services/CustomerService.cs
@@ -0,0 +1,31 @@
+﻿using RetroRealm.Models;
+
+namespace RetroRealm.Data.Services
+{
+    public class CustomerService : ICustomerService
+    {
+        private ApplicationDbContext _context;
+
+        public CustomerService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IQueryable<CustomerModel> GetAll()
+        {
+            IQueryable<CustomerModel> customers = _context.Customers;
+            return customers;
+        }
+
+        public CustomerModel? GetCustomerByEmail(string email)
+        {
+            CustomerModel? customer = _context.Customers.Find(email);
+            return customer;
+        }
+
+        public CustomerModel GetCustomerById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ RetroRealm/Data/Services/ICustomerService.cs
@@ -0,0 +1,11 @@
+﻿using RetroRealm.Models;
+
+namespace RetroRealm.Data.Services
+{
+    public interface ICustomerService
+    {
+        IQueryable<CustomerModel> GetAll();
+        CustomerModel? GetCustomerById(int id);
+        CustomerModel? GetCustomerByEmail(string email);
+    }
+}
