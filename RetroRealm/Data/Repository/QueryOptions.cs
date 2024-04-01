9745ef79455f734edd4165fb82057ce57f836ebc
TreyPoynter <treypoynter@gmail.com>
2024-04-01 18:15:06 -0500

    Create QueryOptions.cs

---
+++ RetroRealm/Data/Repository/QueryOptions.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq.Expressions;
+
+namespace RetroRealm.Data.Repository
+{
+    public class QueryOptions<T>
+    {
+        public Expression<Func<T, Object>> OrderBy { get; set; } = null!;
+        public Expression<Func<T, bool>> Where { get; set; } = null!;
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+
+        private string[] _includes = Array.Empty<string>();
+        public string Includes { 
+            set => _includes = value.Replace(" ", "").Split(',');
+        }
+        public string[] GetIncludes() => _includes;
+
+        public bool HasOrderBy => OrderBy != null;
+        public bool HasWhere => Where != null;
+        public bool HasPaging => PageNumber > 0 && PageSize > 0;
+    }
+}
