56635960d480c3fd62338783912dd166205b3248
TreyPoynter <treypoynter@gmail.com>
2024-04-03 21:13:36 -0500

    Update Program.cs

--- RetroRealm/Program.cs
+++ RetroRealm/Program.cs
@@ -42,6 +42,6 @@ app.UseSession();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}/{slug?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}/{slug?}/{custId?}");
 
 app.Run();
