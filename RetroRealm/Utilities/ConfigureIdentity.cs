fc3667f9a9feb1f83a9986a9c1e71dc98a2415d2
TreyPoynter <treypoynter@gmail.com>
2024-04-16 19:26:15 -0500

    Create ConfigureIdentity.cs

---
+++ RetroRealm/Utilities/ConfigureIdentity.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+using RetroRealm.Models;
+
+namespace RetroRealm.Utilities
+{
+    public class ConfigureIdentity
+    {
+        public static async Task CreateAdminUserASync(IServiceProvider provider)
+        {
+            var roleManager = provider.GetRequiredService<RoleManager<IdentityRole>>();
+            var userManager = provider.GetRequiredService<UserManager<User>>();
+
+            string userName = "admin";
+            string password = "PassW0rd#1";
+            string roleName = "Admin";
+
+            if(await roleManager.FindByNameAsync(roleName) == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole(roleName));
+            }
+            if(await userManager.FindByNameAsync(userName) == null)
+            {
+                User adminUser = new User() { UserName = userName };
+                var result = await userManager.CreateAsync(adminUser, password);
+                if(result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(adminUser, roleName);
+                }
+            }
+        }
+    }
+}
