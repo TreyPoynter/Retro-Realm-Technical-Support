433ae08681983c0cf133f68b61988007fe4dba67
TreyPoynter <treypoynter@gmail.com>
2024-04-17 09:45:10 -0500

    Update TechIncidentController.cs

--- RetroRealm/Controllers/TechIncidentController.cs
+++ RetroRealm/Controllers/TechIncidentController.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using RetroRealm.Data;
 using RetroRealm.Data.Repository;
 using RetroRealm.Models;
 
 namespace RetroRealm.Controllers
 {
+    [Authorize]
     public class TechIncidentController : Controller
     {
         private const string SESSION_KEY = "TechnicianId";
